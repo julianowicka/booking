ef437ab518de7863416325a740eaa09fb0fa9679
Julia Nowicka <julia.nowicka@lemondev.pl>
2025-09-18 14:53:31 +0200

    feat: install and configure Apollo Client with modern @apollo/client package

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { Listings } from "./sections";
 import reportWebVitals from "./reportWebVitals";
 
+const client = new ApolloClient({
+  uri: "/api",
+  cache: new InMemoryCache()
+});
+
 const root = createRoot(document.getElementById("root") as HTMLElement);
-root.render(<Listings title="TinyHouse Listings" />);
+root.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <Listings title="TinyHouse Listings" />
+    </ApolloProvider>
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
